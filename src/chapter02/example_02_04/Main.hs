85f3df5cb5ab002074efc0c89033ada31053791f
José Edil Guimarães De Medeiros <jose.edil@gmail.com>
2018-08-27 15:59:00 -0300

    Add example_02_04

---
+++ src/chapter02/example_02_04/Main.hs
@@ -0,0 +1,105 @@
+-- Bouncing ball example
+
+module Main
+  ( main
+  ) where
+
+import Config
+import Ball
+
+import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Data.Vector
+
+import System.Random
+
+-- RNG and initial setup --
+finiteRandoms :: (RandomGen g, Random a, Num n, Eq n) => n -> g -> ([a], g)
+finiteRandoms 0 gen = ([], gen)
+finiteRandoms n gen =
+  let
+    (value, newGen) = random gen
+    (restOfList, finalGen) = finiteRandoms (n-1) newGen
+  in
+    (value:restOfList, finalGen)
+
+initialMass :: [Float]
+initialMass = fst . finiteRandoms (numBalls) $ mkStdGen 0
+
+initialX :: [Float]
+initialX = map (\_ -> 1) [1..numBalls]
+--initialX = map (\x -> x*(fromIntegral dispWidth)) $ fst . finiteRandoms (numBalls) $ mkStdGen 10
+
+initialY :: [Float]
+initialY = map (\_ -> fromIntegral dispHeight - 1) [1..numBalls]
+--initialY = map (\x -> x*(fromIntegral dispHeight)) $ fst . finiteRandoms (numBalls) $ mkStdGen 20
+
+
+-- | Ball models
+balls :: [Ball]
+balls =
+  let
+    createBall i pos m = Ball i pos nullVector nullVector m
+  in
+    map (\i -> createBall i (initialX!!i, initialY!!i) (10 + 10 * (initialMass !! i))) [0..numBalls-1]
+
+-- | Gravity
+gravity :: Vector
+gravity = (0,-0.01)
+
+-- | Wind
+wind :: Vector
+wind = (0.01,0)
+
+frictionCoeff :: Float
+frictionCoeff = 0.05
+
+-- | Move the ball.
+moveBall :: Ball -> Ball
+moveBall b@(Ball _ _ vel _ m)=
+  let
+    friction = (mulSV frictionCoeff) . (mulSV (-1)) . normalizeVector $ vel
+
+    normalizeVector v
+      | magV v /= 0 = mulSV (1/magV v) v
+      |otherwise = (0,0)
+  in
+    (checkBouncing
+     (fromIntegral dispWidth)
+     (fromIntegral dispHeight)) .
+    updateBall .
+    (applyForce (m `mulSV` gravity)) .
+    (applyForce wind) .
+    (applyForce friction) $ b
+
+moveBalls :: ViewPort -> Float -> [Ball] -> [Ball]
+moveBalls _ _ = map moveBall
+
+renderBalls = pictures . map renderBall
+
+-- | Translate Viewport origin to the left-bottom extreme of the window.
+renderWorld :: [Ball] -> Picture
+renderWorld =
+  let
+    view = ViewPort
+           ((-1)*(fromIntegral $ dispWidth)/2
+           ,(-1)*(fromIntegral $ dispHeight)/2)
+           0
+           1
+  in
+    applyViewPortToPicture view . renderBalls
+
+-- | Simulation
+main :: IO ()
+main =
+  do
+    simulate
+      (InWindow
+       "Nice Window"               -- Window title
+       (dispWidth,dispHeight)      -- Window size
+       (0, 0))                     -- Window position
+      white                         -- Window background
+      simResolution                 -- Time resolution
+      balls                          -- Initial model
+      renderWorld                   -- Model render function
+      moveBalls                      -- Model update function
